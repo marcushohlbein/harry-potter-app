cb31358c1582b1d865d79f6e63be378182f0e032
Marcus Hohlbein <marcus.hohlbein@gmail.com>
2021-02-10 13:00:25 +0100

    Add fetch api

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import AppHeader from './components/AppHeader'
+import getCharacters from './services/getCharacters'
+
+export default function App() {
+  const header = AppHeader('Harry Potter App')
+  document.body.append(header)
+
+  getCharacters()
+    .then(data => console.log(data))
+    .catch(error => console.log(error))
+}
