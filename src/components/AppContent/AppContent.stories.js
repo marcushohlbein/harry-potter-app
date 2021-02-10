526081934c97b082b4048fda87578c94aa35a36a
Marcus Hohlbein <marcus.hohlbein@gmail.com>
2021-02-10 16:13:45 +0100

    Fixings after review

---
+++ src/components/AppContent/AppContent.stories.js
@@ -0,0 +1,8 @@
+import AppContent from './AppContent'
+
+export default {
+  title: 'AppContent',
+  component: AppContent,
+}
+
+export const defaultAppContent = () => AppContent()
--- src/components/Content/Content.stories.js
+++
@@ -1,8 +0,0 @@
-import Content from './Content'
-
-export default {
-  title: 'Content',
-  component: Content,
-}
-
-export const defaultContent = () => Content()
