ddb53c8255d123368499966f0bf271dfe52209da
Marcus Hohlbein <marcus.hohlbein@gmail.com>
2021-02-11 15:31:15 +0100

    Add housefilter logic

--- src/components/HouseFilter/HouseFilter.js
+++ src/components/HouseFilter/HouseFilter.js
@@ -1,3 +1,17 @@
+import createElement from '../../lib/createElement'
+import Button from '../Button/Button'
 import './HouseFilter.css'
 
-export default function HouseFilter(onclick) {}
+export default function HouseFilter(onFilterByHouse) {
+  const houses = ['Gryffindor', 'Ravenclaw', 'Slytherin', 'Huffelpuff']
+
+  const buttons = houses.map(house => {
+    const button = Button(house, () => {
+      onFilterByHouse(house)
+      button.toggle()
+    })
+    return button
+  })
+
+  return createElement('section', { className: 'HouseFilter' }, ...buttons)
+}
