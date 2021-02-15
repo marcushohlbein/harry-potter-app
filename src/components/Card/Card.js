bc4372c2b52f1f272a88d065090fe3663886daf3
Marcus Hohlbein <marcus.hohlbein@gmail.com>
2021-02-15 10:00:54 +0100

    Add logic for colored cards by house

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,8 +1,27 @@
 import createElement from '../../lib/createElement'
 import './Card.css'
 
-export default function Card(...personData) {
-  const el = createElement('section', { className: 'Card' }, ...personData)
+export default function Card(name, house) {
+  function getHouseColor(house) {
+    if (house === 'Gryffindor') {
+      return '#EF5350'
+    } else if (house === 'Huffelpuff') {
+      return '#FED47E'
+    } else if (house === 'Slytherin') {
+      return '#8AC185'
+    } else if (house === 'Ravenclaw') {
+      return '#1E88E5'
+    }
+  }
+
+  const el = createElement(
+    'section',
+    {
+      className: 'Card',
+      backgroundColor: getHouseColor(house),
+    },
+    name
+  )
 
   return el
 }
